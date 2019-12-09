331f2d49657390e8e2d6ec2afed21da21f7e02b5
kiksekage <thomas.nyegaard.signori@gmail.com>
2019-12-09 12:09:00 +0100

    maybe it works, decreasing loss function, grus and loss functions changed

--- embeddings.py
+++ embeddings.py
@@ -8,15 +8,15 @@ import torch.optim as optim
 SOS_token = 0
 EOS_token = 1
 
-class Input:
+class Lang:
     def __init__(self, name):
         self.name = name
         self.word2index = {}
         self.word2count = {}
-        #self.index2word = {SOS_token: "SOS", EOS_token: "EOS"}
-        self.index2word = {}
-        #self.n_words = 2  # Count SOS and EOS
-        self.n_words = 0
+        self.index2word = {SOS_token: "SOS", EOS_token: "EOS"}
+        #self.index2word = {}
+        self.n_words = 2  # Count SOS and EOS
+        #self.n_words = 0
 
     def addSentence(self, sentence):
         for word in sentence.split(' '):
@@ -30,35 +30,6 @@ class Input:
             self.n_words += 1
         else:
             self.word2count[word] += 1
-
-class Output:
-    def __init__(self, name):
-        self.name = name
-        self.word2index = {}
-        self.word2count = {}
-        #self.index2word = {SOS_token: "SOS", EOS_token: "EOS"}
-        self.index2word = {}
-        #self.n_words = 2  # Count SOS and EOS
-        self.n_words = 0
-
-    def addSentence(self, sentence):
-        for word in sentence.split(' '):
-            self.addWord(word)
-
-    def addWord(self, word):
-        if word not in self.word2index:
-            self.word2index[word] = self.n_words
-            self.word2count[word] = 1
-            self.index2word[self.n_words] = word
-            self.n_words += 1
-        else:
-            self.word2count[word] += 1
-
-
-        
-def get_embedding(word, lookup_dict, embeds):
-    tensor = torch.tensor([lookup_dict[word]], dtype=torch.long)
-    return embeds(tensor)
 
 
 def indexesFromSentence(lang, sentence):
---
+++ layers_attempt.py
@@ -0,0 +1,153 @@
+import torch
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from data_loader import *
+from embeddings import *
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class CustomLoss(torch.autograd.Function):  
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        return
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        import ipdb; ipdb.set_trace()
+        #pass
+        return
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(Encoder, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size) # num_layers=2,
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        #CustomLoss.forward(input)
+
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+class Decoder(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(Decoder, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size) #num_layers=2
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+#loss = CustomLoss.apply
+
+def train(input_tensor, output_tensor, encoder, encoder_optimizer, decoder, decoder_optimizer, criterion, max_length=100, clipping_value=5):
+    encoder_hidden = encoder.initHidden()
+
+    encoder_optimizer.zero_grad()
+
+    input_length = input_tensor.size(0)
+    output_length = output_tensor.size(0)
+
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+    loss = 0
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+    
+    decoder_input = torch.tensor([[SOS_token]], device=device)
+
+    decoder_hidden = encoder_hidden
+
+    for di in range(output_length):
+        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+        topv, topi = decoder_output.topk(1)
+        decoder_input = topi.squeeze().detach()  # detach from history as input
+        loss += criterion(decoder_output, output_tensor[di])
+
+        if decoder_input.item() == EOS_token:
+            break
+
+    #loss = CustomLoss
+    loss.backward()
+
+    torch.nn.utils.clip_grad_norm_(decoder.parameters(), clipping_value)
+    torch.nn.utils.clip_grad_norm_(encoder.parameters(), clipping_value)
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+    
+    return loss.item() / output_length
+
+    
+def trainIters(encoder, decoder, train_data, input_lang, output_lang, learning_rate=0.001):
+    encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
+    criterion = nn.NLLLoss()
+
+    losses = []
+    print(train_data.shape[0])
+    print_loss_total = 0
+
+    for iter in range(train_data.shape[0]):
+        training_pair = tensorsFromPair(train_data[iter], input_lang, output_lang)
+        input_tensor = training_pair[0]
+        output_tensor = training_pair[1]
+
+        loss = train(input_tensor, output_tensor, encoder, encoder_optimizer, decoder, decoder_optimizer, criterion)
+        losses.append(loss)
+        print_loss_total += loss
+
+        if iter % 100 == 0:
+            print_loss_avg = print_loss_total / 100
+            print(print_loss_avg)
+            print_loss_total = 0
+
+    return losses
+
+
+
+dl = DataLoader("SCAN")
+train_data, test_data = dl.load_1a()
+
+train_in = Lang("train_input")
+train_out = Lang("train_output")
+
+test_in = Lang("test_input")
+test_out = Lang("test_output")
+
+for datapoint in train_data:
+        train_in.addSentence(datapoint[0])
+        train_out.addSentence(datapoint[1])
+
+for datapoint in test_data:
+        test_in.addSentence(datapoint[0])
+        test_out.addSentence(datapoint[1])
+
+encoder = Encoder(train_in.n_words, 200)
+decoder = Decoder(200, train_out.n_words)
+
+losses = trainIters(encoder, decoder, train_data, train_in, train_out)
